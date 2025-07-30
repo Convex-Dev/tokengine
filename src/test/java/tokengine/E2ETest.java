d3ee95be59839971625f392ad53292f440742bd0
mikera <mike@mikera.net>
2025-07-30 15:17:24 +0100

    More test classes

---
+++ src/test/java/tokengine/E2ETest.java
@@ -0,0 +1,15 @@
+package tokengine;
+
+import org.junit.jupiter.api.Test;
+
+public class E2ETest {
+
+	/**
+	 *  This is a USDC transaction on Sepolia to the defined receiver account in confio-test.json:
+	 *  - 0x06eEB4bb0BC58671d097824611F76fe50C5dB075
+	 */
+	
+	@Test public void testPublicDeposit() {
+		
+	}
+}
---
+++ src/test/java/tokengine/TestServer.java
@@ -0,0 +1,13 @@
+package tokengine;
+
+import tokengine.client.Client;
+
+public class TestServer {
+
+	// TODO:
+	public static int TEST_PORT=8999;
+	public static Engine engine=null;
+	public static APIServer server=null;
+	public static Client client=null;
+
+}
